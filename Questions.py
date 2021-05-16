48d47963d15760b9bbdb3ff7c2ad067e0dbd7013
Leo Bowen Wang <59091252+leobowenwang@users.noreply.github.com>
2021-05-16 16:32:49 +0200

    Increased Question Pool, updated Impressum and Features

--- Questions.py
+++ Questions.py
@@ -6,10 +6,51 @@ class Questions:
         self.questionList = []
 
     def createQuestions(self):
-        self.addQuestion(Question.Question("Ist der Vertragsabschluss länger als 14 Tage her?","Ok","Nein"))
-        self.addQuestion(Question.Question("Wurden Sie über das Bestehen eines Rücktrittsrechts informiert?","Wurde ich","Wurde ich nicht"))
+        #0
+        self.addQuestion(
+            Question.Question("Zu welchem Zweck haben Sie die Ware gekauft?",
+                                           "Ich habe die Ware für private Zwecke gekauft oder plane, ein Unternehmen zu gründen und benötige die Ware zur Aufnahme des Betriebs dieses Unternehmens.",
+                                           "Ich betreibe bereits ein Unternehmen und benötige zum Betrieb des Unternehmens die Ware."))
+        #1
+        self.addQuestion(
+            Question.Question("Haben Sie die Ware von einem Unternehmer gekauft?",
+                                           "Mein*e Vertragspartner*in verkauft die Ware in einem auf Dauer angelegtem System (Online-Shop) und operiert scheinbar zumindest auf Kostendeckungsbasis, um diesen Betrieb weiter aufrecht zu erhalten.",
+                                           "Es handelt sich nur um eine Privatperson – sie verkauft die Ware, ohne dass Absicht auf eine andauernde Erwerbstätigkeit auf diesem Weg erkennbar ist."))
+        #2
+        self.addQuestion(
+            Question.Question("Erfolgte der Kauf über ein dafür organisiertes System und ohne körperliche Anwesenheit?",
+                              "Es handelt sich um einen Webshop, welcher genau für diesen Vertriebszweck geschaffen wurde, außerdem war keine der Parteien bei der Bestellung anwesend. (Es handelt sich um einen Fernabsatzvertrag gem §3 Z 2 FAGG)",
+                              "Die Ware wurde zwar elektronisch bestellt, der gewählte Weg wurde aber nur als Ausnahme genutzt oder der Weg ist für den Vertrieb auf diese Weise von der anderen Partei nicht beabsichtigt oder die Bestellung erfolgte unter Anwesenheit beider Parteien."))
+        #3
+        self.addQuestion(
+            Question.Question("Ist die Rücktrittsfrist von 14 Tagen verstrichen?",
+                                           "Ich habe die Lieferung erhalten Seit der Lieferung sind bereits mehr als 14 Tage verstrichen.",
+                                           "Die Rücktrittsfrist von 14 Tagen ist noch nicht verstrichen."))
+        #4
+        self.addQuestion(
+            Question.Question("Haben Sie innerhalb von 14 Tage nach Erhalt der Ware dem/der Verkäufer*in mitgeteilt, dass Sie vom Vertrag zurücktreten möchten?",
+                              "Ich habe den/die Verkäufer*in von meinem Rücktritt informiert. Dieser muss keiner bestimmten Form entsprechen und kann auch über ein Formular auf der Website des Unternehmens erfolgt sein.",
+                              "Eine solche Erklärung habe ich nicht abgegeben."))
+        #5
+        self.addQuestion(
+            Question.Question(
+                "Wurden Sie über Ihr gesetzliches Rücktrittsrecht vor der Bestellung informiert?",
+                "Die Widerrufsbelehrung erfolgte entweder auf einer der Seiten vor dem Bestellbutton oder innerhalb der allgemeinen Geschäftsbedingungen. Dort wurde die Belehrung direkt angeführt oder verlinkt. Mir wurde auch ein Muster-Widerrufsformular bereitgestellt, beziehungsweise habe ich mit dem Erhalt der Lieferung Zugang zu diesen Informationen erhalten. (gem §7 FAGG)",
+                "Eine Widerrufsbelehrung wurde weder auf einer Seite vor dem Bestellbutton, in den AGB, noch mit der Lieferung bereitgestellt. Selbst wenn eine Belehrung vorliegt, wurde kein Widerrufsformular zur Verfügung gestellt. Die Widerrufsbelehrung ist auch nicht nach Erhalt der Lieferung erfolgt."))
+        #6
+        self.addQuestion(
+            Question.Question(
+                "Erfolgte eine nachträgliche Widerrufsbelehrung?",
+                "Es erfolgte eine nachträgliche Widerrufsbelehrung innerhalb von zwölf Monaten ab Lieferung",
+                "Eine nachträgliche Widerrufsbelehrung innerhalb von zwölf Monaten ab Lieferung ist nicht erfolgt."))
+        # 7
+        self.addQuestion(
+            Question.Question("Sind seit der Lieferung mehr als ein Jahr und 14 Tage verstrichen?",
+                              "Seit dem Erhalt der Lieferung sind bereits mehr als zwölf Monate und 14 Tage verstrichen",
+                              "Die Frist von zwölf Monaten und 14 Tagen ab Lieferung ist noch nicht verstrichen."))
 
-    def addQuestion(self,question):
+
+    def addQuestion(self, question):
         self.questionList.append(question)
 
     def jsonable(self):
--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class ShoppingBuddy(KnowledgeEngine):
         self.declare(Result(resultValue=True,
                             law="Sie handeln als Verbraucher*in im Sinne des § 1 KSchG. Dadurch kann der Kaufvertrag gemäß § 1 FAGG von der Geltung des FAGG erfasst sein und möglicherweise von Ihrem Rücktrittsrecht gemäß § 11 FAGG Gebrauch machen."))
         self.halt()
+
     @Rule(Fact(frage0="false"), salience=0)
     def frage0false(self):
         self.declare(Result(resultValue=False,
@@ -47,7 +48,7 @@ class ShoppingBuddy(KnowledgeEngine):
                             law="Bei dem Kauf handelt es sich nicht um einen Fernabsatzvertrag gem §3 Z 2 FAGG, da der Kauf nicht über ein dafür organisiertes System und/oder ohne körperlichen Anwesenheit erfolgt ist. Für alternative Rücktrittsmöglichkeiten und eine detaillierte Prüfung Ihrer Rechtsfrage können Ihnen folgende Anwaltskanzleien weiterhelfen:"))
         self.halt()
 
-    @Rule(Fact(frage0="true"), Fact(frage1="true"), Fact(frage2="true"),Fact(frage3="true"), salience=3)
+    @Rule(Fact(frage0="true"), Fact(frage1="true"), Fact(frage2="true"), Fact(frage3="true"), salience=3)
     def frage3true(self):
         self.declare(Result(resultValue=True,
                             law="Ich habe die Lieferung erhalten Seit der Lieferung sind bereits mehr als 14 Tage verstrichen."))
@@ -56,14 +57,64 @@ class ShoppingBuddy(KnowledgeEngine):
     @Rule(Fact(frage0="true"), Fact(frage1="true"), Fact(frage2="true"), Fact(frage3="false"), salience=3)
     def frage3false(self):
         self.declare(Result(resultValue=True,
-                            law="Die Rücktrittsfrist von 14 Tagen ist noch nicht verstrichen."))
+                            law=""))
+        self.halt()
+
+    @Rule(Fact(frage0="true"), Fact(frage1="true"), Fact(frage2="true"), Fact(frage3="true"), Fact(frage4="true"),
+          salience=4)
+    def frage4true(self):
+        self.declare(Result(resultValue=False,
+                            law="Sie haben gem. § 15 Abs 1 FAGG ab dem Tag, an dem Sie die Rücktrittserklärung abgesendet haben, 14 Tage Zeit, die erhaltene Ware zurückzusenden. Der/die Unternehmer*in hat Ihnen gem. § 14 FAGG unverzüglich, spätestens jedoch binnen 14 Tagen ab Zugang der Rücktrittserklärung, die geleisteten Zahlungen zu erstatten. Er hat für die Rückzahlung dasselbe Zahlungsmittel zu verwenden, dessen Sie sich bei der damaligen Zahlung bedient haben. Eine Abweichung davon ist nur möglich, wenn Ihnen dadurch keine Kosten entstehen und sie ausdrücklich zustimmen. Versandkosten müssen Ihnen vollständig ersetzt werden, außer Sie haben sich gegen die günstigste Standardlieferung entschieden. Dann muss Ihnen die Differenz zwischen Standardlieferung und gewählter Versandart nicht ersetzt werden."))
+        self.halt()
+
+    @Rule(Fact(frage0="true"), Fact(frage1="true"), Fact(frage2="true"), Fact(frage3="true"), Fact(frage4="false"),
+          salience=4)
+    def frage4false(self):
+        self.declare(Result(resultValue=True, law=""))
+        self.halt()
+
+    @Rule(Fact(frage0="true"), Fact(frage1="true"), Fact(frage2="true"), Fact(frage3="true"), Fact(frage4="false"), Fact(frage5="true"),
+          salience=5)
+    def frage5true(self):
+        self.declare(Result(resultValue=True, law=""))
+        self.halt()
+
+    @Rule(Fact(frage0="true"), Fact(frage1="true"), Fact(frage2="true"), Fact(frage3="true"), Fact(frage4="false"), Fact(frage5="false"),
+          salience=5)
+    def frage5false(self):
+        self.declare(Result(resultValue=True, law="Sie wurden nicht (rechtzeitig) über das Rücktrittsrecht belehrt, daher verlängert sich die Rücktrittsfrist um 12 Monate gem §12 Abs 1 FAGG. Erteilt der/die Unternehmer *in die Informationen verspätet, aber innerhalb der 12 Monatsfrist, so endet die Rücktrittsfrist binnen 14 Tagen (die Frist beginnt am Tag, an dem der verbraucher dieser Information erhalten hat)."))
+        self.halt()
+
+    @Rule(Fact(frage0="true"), Fact(frage1="true"), Fact(frage2="true"), Fact(frage3="true"), Fact(frage4="false"), Fact(frage5="false"), Fact(frage6="true"),
+          salience=6)
+    def frage6true(self):
+        self.declare(Result(resultValue=True, law=""))
+        self.halt()
+
+    @Rule(Fact(frage0="true"), Fact(frage1="true"), Fact(frage2="true"), Fact(frage3="true"), Fact(frage4="false"), Fact(frage5="false"), Fact(frage6="false"),
+          salience=6)
+    def frage6false(self):
+        self.declare(Result(resultValue=True, law=""))
+        self.halt()
+
+    @Rule(Fact(frage0="true"), Fact(frage1="true"), Fact(frage2="true"), Fact(frage3="true"), Fact(frage4="false"), Fact(frage5="false"), Fact(frage6="false"), Fact(frage7="true"),
+          salience=7)
+    def frage7true(self):
+        self.declare(Result(resultValue=True, law=""))
+        self.halt()
+
+    @Rule(Fact(frage0="true"), Fact(frage1="true"), Fact(frage2="true"), Fact(frage3="true"), Fact(frage4="false"), Fact(frage5="false"), Fact(frage6="false"), Fact(frage7="false"),
+          salience=7)
+    def frage7false(self):
+        self.declare(Result(resultValue=True, law=""))
         self.halt()
 
     # print("sie können zurücktreten")
-    @Rule(salience=-1)
+    @Rule(salience=0)
     def rücktritt(self):
         self.declare(Result(resultValue=True,
                             law="Ihren Angaben nach haben Sie die Rücktrittsmöglichkeit nach § 11 FAGG. Da es sich hierbei aber um eine Vorabschätzung handelt, wird die Beratung von Rechtsanwält*innen empfohlen. Die folgenden Ansprechpersonen stehen Ihnen hierfür zur Verfühung: (PLZ eingeben) → Liste von kooperierenden Anwält*innen"))
+        self.halt()
 
 
 """
@@ -87,8 +138,8 @@ def get_response(answers):
     for i in range(len(answers)):
         engine.declare(Fact(**{'frage' + str(i): answers[i]}))
     engine.run()
-    result = list(engine.facts.get(engine.facts.last_index-1).values())
-    return {"value":str(result[0]),"law":result[1]}
+    result = list(engine.facts.get(engine.facts.last_index - 1).values())
+    return {"value": str(result[0]), "law": result[1]}
 
 
 def formatRequest(request):
